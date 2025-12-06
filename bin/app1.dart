714c4fbc8c180d4347a315097a877f26b5b3d200
strong4124 <abdoulrachidzongo506@gmail.com>
2025-12-06 23:09:57 +0000

    code Dart app code du samedi (06/12/2025)

--- bin/app1.dart
+++ bin/app1.dart
@@ -4,21 +4,27 @@ import '../lib/models/compte_utilisateur.dart';
 void main (){
 
 
-Cu c1 = const Cu(
+CompteUtilisateur c1 = const CompteUtilisateur(
   name : "ISSAKA",
   username : "Abdoul Razak",
   email : "arissaka@esmt.com",
-  id : 100,
+  id : 10,
 );
 
 
-Cu c2 = const Cu (
+CompteUtilisateur c2 = const CompteUtilisateur (
   name: "ISSAKA",
   username: "Abdoul Razak",
   email: "arissaka@esmt.com",
   id : 100 ,
 
 );
+
+// les instances qui auront les mm valeur seront mis dans la mm add memoire il seront comme un seul objet (const)
+
+// si id de c1 et c2 sont egales ils seront dans la mm add memoire donc le resultat sera true
+// si id de c1 et c2 sont differents ils seront dans la mm add memoire donc le resultat sera false
+
 
   print(c1 == c2);
   print(identical (c1,c2));
--- lib/models/compte_utilisateur.dart
+++ lib/models/compte_utilisateur.dart
@@ -1,4 +1,4 @@
-class Cu{
+class CompteUtilisateur{
 
   final String name ;
   final String username ;
@@ -11,7 +11,7 @@ les instances qui auront les mm valeur seront mis dans la mm add
 memoire il seront comme un seul objet (const)
 ce qui permet une optimisation de la mémoire 
  */
-const Cu({ 
+const CompteUtilisateur({ 
   required this.name,
   required this.username,
   required this.email,
